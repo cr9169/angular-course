61686ecb55f7c362737a529b0434cd85a14fc51a
cr9169 <bar.gabay.home@gmail.com>
2025-02-01 20:11:08 +0200

    feat: finished Section 2, video number 10

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
 
 @Component({
@@ -12,4 +12,11 @@ export class CourseCardComponent {
     required: true,
   })
   course: Course;
+
+  @Output("courseSelected")
+  courseEmitter = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    this.courseEmitter.emit(this.course);
+  }
 }
