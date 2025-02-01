16e21a8adf92bf5b899380615014e63139244669
cr9169 <bar.gabay.home@gmail.com>
2025-02-01 23:22:09 +0200

    after video 13

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -13,6 +13,11 @@ export class CourseCardComponent {
   })
   course: Course;
 
+  @Input({
+    required: true,
+  })
+  index: number;
+
   @Output("courseSelected")
   courseEmitter = new EventEmitter<Course>();
 
