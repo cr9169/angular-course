fde255146f80cdbbfbc470d181f37f6766a80dc4
cr9169 <bar.gabay.home@gmail.com>
2025-02-02 11:21:46 +0200

    after video 21

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "course-card",
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./course-card.component.html",
   styleUrl: "./course-card.component.css",
 })
@@ -23,5 +24,9 @@ export class CourseCardComponent {
 
   onCourseViewed() {
     this.courseEmitter.emit(this.course);
+  }
+
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
   }
 }
--- src/db-data.ts
+++ src/db-data.ts
@@ -1,4 +1,6 @@
 export const COURSES: any = [
+  undefined,
+  null,
   {
     id: 1,
     description: "Angular Core Deep Dive",
