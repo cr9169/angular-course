faa7cd49d186f8b5431eacd39cf9f8d7ca0fc709
cr9169 <bar.gabay.home@gmail.com>
2025-02-02 19:53:40 +0200

    after video 33

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
 import { COURSES } from "../db-data";
 import { Course } from "./model/course";
 import { CourseCardComponent } from "./course-card/course-card.component";
@@ -12,17 +19,27 @@ import { CourseCardComponent } from "./course-card/course-card.component";
 export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
-  @ViewChild("cardRef", { read: ElementRef })
-  card: CourseCardComponent;
-
-  @ViewChild("courseImage")
-  courseImage: ElementRef;
+  @ViewChildren(CourseCardComponent)
+  cards: QueryList<CourseCardComponent>;
 
   constructor() {}
 
   ngAfterViewInit(): void {
-    console.log("courseImage", this.courseImage);
+    this.cards.changes.subscribe((cards) => console.log(cards));
   }
 
   onCourseSelected(course: Course) {}
+
+  onCourseEdited() {
+    this.courses.push({
+      id: 1,
+      description: "Angular core deep dive",
+      iconUrl:
+        "https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png",
+      longDescription:
+        "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+      category: "INTERMEDIATE",
+      lessonsCount: 10,
+    });
+  }
 }
