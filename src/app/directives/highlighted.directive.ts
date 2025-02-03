2f02ccd5836161a25c86ca311f58633d6dad071c
cr9169 <bar.gabay.home@gmail.com>
2025-02-03 15:22:47 +0200

    after video 41

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from "@angular/core";
+import { Directive, HostBinding, Input } from "@angular/core";
 
 @Directive({
   selector: "[highlighted]",
 })
 export class HighlightedDirective {
-  constructor() {
-    console.log("Directive created!");
+  @Input() highlighted: boolean = false;
+
+  @HostBinding("class.highlighted")
+  get cssClasses() {
+    return this.highlighted;
+  }
+
+  @HostBinding("attr.disabled")
+  get disabled() {
+    return true;
   }
 }
